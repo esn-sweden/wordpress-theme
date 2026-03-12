8609b28029913e09f9222eec1a2a060ea2414018
Elias Jollivet <63159238+Lurbyte@users.noreply.github.com>
2026-03-12 02:49:52 +0100

    Import current theme

---
+++ patterns/applications-are-open.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Applications are open
+ * Slug: esn-sweden/applications-are-open
+ * Categories: 
+ */
+?>
+<!-- wp:paragraph -->
+<p><strong>Applications are now open!</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Don’t miss your chance—<strong>apply by 22 January</strong> for an unforgettable experience! Check out the positions and more info below to find the perfect role for you.</p>
+<!-- /wp:paragraph -->
---
+++ patterns/footer-copy.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Title: footer-copy
+ * Slug: esn-sweden/footer-copy
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"width":"160px","sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image size-medium is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/web-SE-LUND-ESN-colour-black-300x167.png" alt="" class="" style="width:160px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:social-links {"align":"right","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<ul class="wp-block-social-links alignright"><!-- wp:social-link {"url":"https://www.instagram.com/esn_lund/","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://www.facebook.com/ESNLund/","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
+<div class="wp-block-column" style="flex-basis:33%"><!-- wp:group {"style":{"dimensions":{"minHeight":""},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Erasmus Student Network Lund<br>Sandgatan 2<br>223 50 Lund</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>Office hours</strong><br>Monday: 15:00–17:00<br>Wednesday: 11:00–13:00<br>Friday: 12:00–14:00</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":""},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong><a href="https://esnlund.org/contact/" data-type="page" data-id="52">Contact us</a></strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33%"} -->
+<div class="wp-block-column" style="flex-basis:33%"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">External links</h5>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"0"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33%"} -->
+<div class="wp-block-column" style="flex-basis:33%"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="min-height:100%"><!-- wp:search {"label":"","showLabel":false,"placeholder":"Looking for something?","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"right","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<p class="has-text-align-right"><strong>©</strong> 2024 Erasmus Student Network Lund</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
