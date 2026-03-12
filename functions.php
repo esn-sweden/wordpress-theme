354c2b73fe73b4367a61e7c411b39dd0722d250a
Elias Jollivet <63159238+Lurbyte@users.noreply.github.com>
2026-03-12 03:18:22 +0100

    Remove defunct loading of fonts

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function enqueue_custom_styles() {
-    wp_enqueue_style('custom-fonts', get_stylesheet_directory_uri() . '/css/kelson-sans.css');
-
-    add_editor_style('/var/www/wordpress/wp-content/themes/esnsweden/css/editor-styles.css');
-}
-add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
